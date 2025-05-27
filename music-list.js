164814283c55673db22fc34b7e3f03a664900688
vien2k3 <89335397+vien2k3@users.noreply.github.com>
2025-05-27 08:48:39 +0700

    first project

---
+++ music-list.js
@@ -0,0 +1,45 @@
+
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+ 
+ 
+  {
+    name: "Về Bên Anh",
+    artist: "Jena cover",
+    img: "ve-ben-anh",
+    src: "ve-ben-anh"
+  },
+  {
+    name: "Tham Phú Phụ Bần",
+    artist: "Thanh Tình cover",
+    img: "tham-phu-phu-ban",
+    src: "tham-phu-phu-ban"
+  },
+  {
+    name: "Cẩm Tú Cầu",
+    artist: "Kiều Chi cover",
+    img: "cam-tu-cau",
+    src: "cam-tu-cau"
+  },
+  {
+    name: "Chắc Ai Đó Sẽ Về",
+    artist: "Sơn Tùng M-TP",
+    img: "chac-ai-do-se-ve",
+    src: "chac-ai-do-se-ve"
+  },
+  {
+    name: "Đơn Phương Mình Anh",
+    artist: "Linh Tuổi Thìn",
+    img: "don-phuong-minh-anh",
+    src: "don-phuong-minh-anh"
+  },
+  
+];
